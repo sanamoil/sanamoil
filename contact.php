b2708182619d9796a5cc18cd77d41971df728269
sanamoil <shahdbelhaj87@gmail.com>
2023-12-24 00:19:54 +0200

    sanamoil

---
+++ contact.php
@@ -0,0 +1,50 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Get the form fields and remove whitespace
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r", "\n"), array(" ", " "), $name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $subject = str_replace(array("\r", "\n"), array(" ", " "), $subject);
+    $message = trim($_POST["message"]);
+
+    // Check that data was sent to the mailer
+    if (empty($name) or empty($subject) or empty($message) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo "Please complete the form and try again.";
+        exit;
+    }
+
+    // Set the recipient email address
+    $recipient = "shahdfbelhaj@gmail.com";
+
+    // Set the email subject
+    $email_subject = "$subject";
+
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Subject: subject\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $email_subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    // Not a POST request, set a 403 (forbidden) response code
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+
+?>
